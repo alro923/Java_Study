bb505c554bac74e2bc8edcf2b6ee1709b4f61cc0
alro923 <alro923@naver.com>
2019-09-12 20:20:26 +0900

    chapter 1

---
+++ src/ch01/src/Hello.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 한 줄 주석은 이렇게 작성한다.
+		
+		/*
+			범위 주석은 이렇게 작성한다.
+		*/
+		
+		System.out.println("Hello, Java!");
+		//println의 ln은 'line'이란 뜻이다. 따라서 println을 써서 출력한 구문은 항상 개행된다!
+		
+		
+	}
+   
+}

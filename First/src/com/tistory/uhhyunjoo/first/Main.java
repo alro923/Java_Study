c2bc713d4468fee4cdd65b9696115d64cde818b1
uhhyunjoo <alro923@naver.com>
2021-04-11 23:06:49 +0900

    🔨 Add First project

---
+++ First/src/com/tistory/uhhyunjoo/first/Main.java
@@ -0,0 +1,9 @@
+package com.tistory.uhhyunjoo.first;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World!");
+    }
+}

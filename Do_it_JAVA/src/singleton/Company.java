9b27aef102c02f596b483fb8893c15abb6ba7eb6
uhhyunjoo <alro923@naver.com>
2021-04-24 22:05:52 +0900

    🔨 Add singleton Part

--- Do_it_JAVA/src/singleton/Company.java
+++ Do_it_JAVA/src/singleton/Company.java
@@ -4,11 +4,18 @@ public class Company {
 
     private static Company instance = new Company();
     // 접근 x : private, 여러개 아니고 하나만 : static
+    // 이 instance 가, 전체(+외부)에서 유일하게 사용될 인스턴스임
+    // 함부로 null이된다거나, 값이 바뀌면 안되니까 private으로 선언
+    // 여러개 있을게 아니고, 딱 하나만 있을거니까 static으로 선언
+    // 이 instance 를 외부에서 가져다 쓸 수 없으니까, public 으로 getIstance 만들어줌
     private Company(){};
+    // default 가 public이니까, private으로 만들어버림
 
     public static Company getInstance(){
         if (instance == null)
             instance = new Company();
         return instance;
     }
+    // 메서드를 public 으로 제공하면, 생성된 instance를 외부에서 접근 가능
+    // 객체를 생성하지 않고 이 메서드를 부르기 위해, 이 메서드를 static으로 선언한다.
 }
--- Do_it_JAVA/src/singleton/CompanyTest.java
+++ Do_it_JAVA/src/singleton/CompanyTest.java
@@ -4,13 +4,25 @@ import java.util.Calendar;
 
 public class CompanyTest {
     public static void main(String[] args) {
+
+        // Company c1 = new Company();
+        // Company c2 = new Company();
+        // 이렇게 하면 서로 다른 인스턴스 여러개 생성하는 거잖아...
+        // 딱 하나만 만들고 싶어!!
+
         Company c1 = Company.getInstance();
         Company c2 = Company.getInstance();
 
         // Company c3 = new Company(); // error
         System.out.println(c1 == c2); // true
-        System.out.println(c2 );
+        System.out.println(c1);
+        System.out.println(c2); // 주소값이 같음을 알 수 있다.
 
+        // Calendar 클래스도 singleton 패턴으로 구현되어 있다.
+        // new 해보면, constructor 가 없고, getInstance()가 있다.
+        // 시간이란 건 여러개가 아니고, 하나만 있기 때문에 singleton 으로 제공한다.
+        // 시간은 다 같으니까, 여러개를 만들 필요는 없다!
+        // (sout 해보면 false 나오는데, 만들어진 시간이 달라서 그렇다)
         Calendar cal = Calendar.getInstance();
     }
 }
